445ae5bd061ca77a76c6c3c1ec2d6d16d03b8fb9
Nicolas de Oliveira <nicolas.lima2108@gmail.com>
2026-05-20 21:20:01 -0300

    Criação do Codigo Data

---
+++ ApiIveco/Data/FireBaseData.cs
@@ -0,0 +1,44 @@
+﻿using Google.Cloud.Firestore;
+using System.Text.Json;
+
+namespace ApiIveco.Data
+{
+    public class FireBaseData
+    {
+        public FirestoreDb Db { get; private set; }
+
+        public FireBaseData()
+        {
+
+            var chave = "chave_API/firebase-key.json";
+            var fullPath = Path.Combine(AppContext.BaseDirectory, chave);
+
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException($"O arquivo de chave do Firebase não foi encontrado: {fullPath}");
+            }
+
+            string jsonList = File.ReadAllText(fullPath);
+            using JsonDocument doc = JsonDocument.Parse(jsonList);
+
+            if (!doc.RootElement.TryGetProperty("project_id", out var projectIdElement))
+            {
+                throw new Exception("O arquivo de chave do Firebase não contém a propriedade 'project_id'.");
+            }
+
+
+            string projectId = projectIdElement.GetString();
+
+            var builder = new FirestoreDbBuilder
+            {
+
+                ProjectId = projectId,
+                CredentialsPath = fullPath
+
+            };
+
+            Db = builder.Build();
+        }
+
+    }
+}
