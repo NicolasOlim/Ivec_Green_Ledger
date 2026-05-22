81e2045bf2cd16a85870028c7de02ef55df761df
Nicolas de Oliveira <nicolas.lima2108@gmail.com>
2026-05-22 19:54:29 -0300

    Criação da ViewModelBase

---
+++ WpfIveco/ViewModel/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfIveco.ViewModels
+{
+    // Classe base que avisa a tela (XAML) toda vez que uma variável for atualizada
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
