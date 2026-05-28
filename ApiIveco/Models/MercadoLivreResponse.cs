307a5c56a23c4fedbee5bb94b07b373c116ca1d8
Nicolas de Oliveira <nicolas.lima2108@gmail.com>
2026-05-28 19:22:41 -0300

    Conserto do WPF

---
+++ ApiIveco/Models/MercadoLivreResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ApiIveco.Models
+{
+    public class MercadoLivreResponse
+    {
+        [JsonPropertyName("results")]
+        public List<MercadoLivreItem> Results { get; set; }
+    }
+
+    public class MercadoLivreItem
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } // Aqui virá o nome real da peça (Ex: "Filtro De Ar Caminhão Iveco")
+    }
+}
