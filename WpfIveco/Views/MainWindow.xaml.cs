6549d0d8a671eb8e71a419004b8e4576beb5e7e1
erick190813 <erickaraujo1908@gmail.com>
2026-06-15 20:56:11 -0300

    atualizando back-end do wpf

--- WpfIveco/Views/MainWindow.xaml.cs
+++ WpfIveco/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using WpfIveco.ViewModel;
 
@@ -9,16 +13,13 @@ namespace WpfIveco
         public MainWindow()
         {
             InitializeComponent();
-
-            /// LIGA O XAML AO SEU MOTOR LÓGICO
+            /// LIGA O XAML AO SEU MOTOR L GICO
             DataContext = new MainViewModel();
         }
 
         /// <summary>
-        /// --- MÉTODOS DE CONTROLO DA JANELA ---
+        /// --- M TODOS DE CONTROLO DA JANELA ---
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
@@ -43,6 +44,52 @@ namespace WpfIveco
                 this.WindowState = WindowState.Normal;
         }
 
+        /// <summary>
+        /// --- M TODOS DE MÁSCARA E VALIDAÇÃO DE INPUT ---
+        /// </summary>
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
 
+        private void CnpjTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                string apenasDigitos = new string(textBox.Text.Where(char.IsDigit).ToArray());
+
+                if (apenasDigitos.Length > 14)
+                    apenasDigitos = apenasDigitos.Substring(0, 14);
+
+                string cnpjFormatado = "";
+                if (apenasDigitos.Length > 0)
+                {
+                    if (apenasDigitos.Length <= 2)
+                        cnpjFormatado = apenasDigitos;
+                    else if (apenasDigitos.Length <= 5)
+                        cnpjFormatado = $"{apenasDigitos.Substring(0, 2)}.{apenasDigitos.Substring(2)}";
+                    else if (apenasDigitos.Length <= 8)
+                        cnpjFormatado = $"{apenasDigitos.Substring(0, 2)}.{apenasDigitos.Substring(2, 3)}.{apenasDigitos.Substring(5)}";
+                    else if (apenasDigitos.Length <= 12)
+                        cnpjFormatado = $"{apenasDigitos.Substring(0, 2)}.{apenasDigitos.Substring(2, 3)}.{apenasDigitos.Substring(5, 3)}/{apenasDigitos.Substring(8)}";
+                    else
+                        cnpjFormatado = $"{apenasDigitos.Substring(0, 2)}.{apenasDigitos.Substring(2, 3)}.{apenasDigitos.Substring(5, 3)}/{apenasDigitos.Substring(8, 4)}-{apenasDigitos.Substring(12)}";
+                }
+
+                textBox.TextChanged -= CnpjTextBox_TextChanged;
+
+                int posicaoCursorAnterior = textBox.CaretIndex;
+                int tamanhoAnterior = textBox.Text.Length;
+
+                textBox.Text = cnpjFormatado;
+
+                int tamanhoAtual = textBox.Text.Length;
+                int diferencaTamanho = tamanhoAtual - tamanhoAnterior;
+                textBox.CaretIndex = Math.Max(0, Math.Min(tamanhoAtual, posicaoCursorAnterior + diferencaTamanho));
+
+                textBox.TextChanged += CnpjTextBox_TextChanged;
+            }
+        }
     }
 }
